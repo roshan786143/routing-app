f38153b7d4e3620f4a5b402ad05a8c39b300c4e4
roshan786143 <roshanbabu.kng@gmail.com>
2022-12-18 13:15:49 +0530

    added update and delete functionalities

--- routing_app/router.js
+++ routing_app/router.js
@@ -1,15 +1,54 @@
-const express = require('express');
-const route = express.Router(); 
-const accounts = require('./database');
+const express = require("express");
+const route = express.Router();
+let accounts = require("./database");
 
-route.get('/accounts',(req,res)=>{
-    res.json({userData : accounts});
-})
+route.get("/accounts", (req, res) => {
+  res.json({ userData: accounts });
+});
 
-route.post('/accounts',(req,res)=>{
-    const incomingAccount = req.body;
-    accounts.push(incomingAccount);
-    res.json(accounts);
-})
+route.post("/accounts", (req, res) => {
+  const incomingAccount = req.body;
+  accounts.push(incomingAccount);
+  res.json(accounts);
+});
+
+route.get("/accounts/:id", (req, res) => {
+  const accountid = Number(req.params.id);
+  const getAccount = accounts.find((account) => account.id === accountid);
+
+  if (!getAccount) {
+    res.status(500).send("Account Not Found");
+  } else {
+    res.json({ userData: [getAccount] });
+  }
+});
+
+route.put("/accounts/:id", (req, res) => {
+  const accountid = Number(req.params.id);
+  const body = req.body;
+  const account = accounts.find((account) => account.id === accountid);
+  const index = accounts.indexOf(account);
+
+  if (!account) {
+    res.status(500).send("Account Not Found");
+  } else {
+    const updatedAccount = { ...account, ...body };
+    // console.log({...account,...body});
+    accounts[index] = updatedAccount;
+    res.send(updatedAccount);
+  }
+});
+
+route.delete("/accounts/:id", (req, res) => {
+  const accountid = Number(req.params.id);
+  const newAccounts = accounts.filter((account) => account.id!=accountid);
+
+  if (!newAccounts) {
+    res.status(500).send("Account Not Found!");
+  } else {
+    accounts = newAccounts;
+    res.send(accounts);
+  }
+});
 
 module.exports = route;
